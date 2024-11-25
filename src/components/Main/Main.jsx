c60f62c90464811de563751a34a3786edd1f2004
VIKAS GULIA <153659071+VIKASGULIA17@users.noreply.github.com>
2024-11-25 19:37:45 +0530

    responsive and additional div for deletion or edit
    on 24 nov

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,53 +1,95 @@
-import  { useContext } from "react";
+import { useContext, useState } from "react";
 import { assets } from "../../assets/assets";
 import { Context } from "../../context/Context";
 import { AiOutlineLike } from "react-icons/ai";
 import { HiOutlineSpeakerWave } from "react-icons/hi2";
 import { useSpeechSynthesis } from "react-speech-kit";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { FaEdit } from "react-icons/fa";
 const Main = () => {
-  const { onSent, recentPrompt, showResult, loading, resultData, setinput, input } = useContext(Context);
-  //this is for date
-  const date = new Date();
-      const showTime = date.getHours() 
-          + ':' + date.getMinutes() 
-          + ":" + date.getSeconds();
+  const [HiddenDiv, setHiddenDiv] = useState(false);
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setinput,
+    input,
+  } = useContext(Context);
 
-          //this si for speech
-  const {speak}=useSpeechSynthesis();
+  // Date
+  const date = new Date();
+  const showTime =
+    date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+  // Speech synthesis
+  const { speak } = useSpeechSynthesis();
+
   const Convert_to_speech = () => {
     if (resultData) {
       speak({ text: resultData });
-      console.log(resultData)
+      console.log(resultData);
     } else {
       alert("No result data to convert to speech!");
     }
   };
-  
-  
+
   return (
-    <div className="main flex-1 min-h-screen pb-[15vh] relative">
+    <div className="overflow-hidden main flex-1 z-50 min-h-screen pb-[15vh] relative">
+      {HiddenDiv && (
+        <div className="w-[90vw] h-screen fixed z-40 backdrop-blur-sm">
+          <div>
+            <textarea
+              name=""
+              id=""
+              className="border-2 border-black absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-zinc-100 text-black p-4 rounded w-96 h-96"
+            ></textarea>
+          </div>
+          <button
+            className="w-20 h-10 px-4 py-2 bg-red-400 absolute right-[34%] top-[50%] text-white"
+            onClick={() => setHiddenDiv(false)}
+          >
+            Close
+          </button>
+        </div>
+      )}
       {/* Navbar */}
-      <div className="nav flex items-center justify-between text-2xl p-5 text-gray-600">
+      <div className="nav flex items-center justify-between text-xl lg:text-2xl p-2 lg:p-5 text-gray-600">
+        <img src={assets.menu_icon} className="w-7 h-7 lg:hidden" alt="" />
         <p>Ani-gpt</p>
-        <img className="w-12 h-12 border-2 border-black rounded-full" src={assets.user_icon} alt="User Icon" />
+        <img
+          className="h-8 w-8  border-[1px]  lg:w-12 lg:h-12 lg:border-2 border-black rounded-full"
+          src={assets.user_icon}
+          alt="User Icon"
+        />
       </div>
-
       {/* Main Content */}
-      <div className={`main-container max-w-[90vw] mx-5 h-[73vh] rounded-2xl ${!showResult ? 'bg-slate-100' : 'bg-gray-100'} flex flex-col text-center`}>
-
+      <div
+        className={`main-container max-w-[92vw] shadow-lg shadow-zinc-300 lg:max-w-[90vw] mx-5 h-[73vh] rounded-2xl ${
+          !showResult ? "bg-slate-100" : "bg-gray-100"
+        } flex flex-col text-center`}
+      >
         {showResult ? (
           <div className="result py-0 px-[10%] max-h-[70vh] overflow-y-scroll no-scrollbar">
-            <div className="result-title ml-[40vw] px-4 py-2 rounded-lg text-white font-bold text-lg my-10 flex items-center bg-blue-500 gap-5">
-              <img
-                src={assets.user_icon}
-                alt="User Icon"
-                className="w-10 h-10 rounded-full border-2 border-cyan-300"
+            <span className="result-title flex flex-col items-end">
+              <div className="w-auto px-4 py-2 rounded-lg capitalize text-white lg:font-bold font-[400] text-[14px] lg:text-lg my-10 flex items-center bg-blue-500 gap-5">
+                <img
+                  src={assets.user_icon}
+                  alt="User Icon"
+                  className="w-5 h-5 block border-[1.5px] lg:w-10 lg:h-10 rounded-full lg:border-2 border-cyan-300"
+                />
+                <p className="whitespace-nowrap">{recentPrompt}</p>
+              </div>
+              <FaEdit
+                onClick={() => setHiddenDiv(!HiddenDiv)}
+                className="cursor-pointer mb-10 -mt-4 text-gray-600 hover:text-gray-800"
               />
-              <p>{recentPrompt}</p>
-            </div>
+            </span>
+
             <div className="result-data relative flex items-start gap-5">
               {loading ? (
-                <div className="loader  w-full flex flex-col gap-3">
+                <div className="loader w-full flex flex-col gap-3">
                   {Array(3)
                     .fill("")
                     .map((_, index) => (
@@ -59,42 +101,61 @@ const Main = () => {
                 </div>
               ) : (
                 <div>
-
-                <div className="flex bg-purple-400 text-slate-100 py-4 px-3 gap-4 rounded-xl">
-                  <img src={assets.user_icon} alt="Gemini Icon" className="w-10 h-10 rounded-full " />
-
-                <p
-                dangerouslySetInnerHTML={{ __html: resultData }}
-                className="text-[17px] w-[60vw ] text-justify pt-2 font-normal leading-6"
-                ></p>
-                
-                </div>
-                <div className="bg-green-300 flex justify-between">
-                  <div className="flex gap-4 py-2">
-                    <img src={assets.Copy_text} alt="" className="w-5 h-5 cursor-pointer"/>
-                    <AiOutlineLike className="cursor-pointer"/>
-                    <HiOutlineSpeakerWave className="cursor-pointer" onClick={() => Convert_to_speech()} />
-
-                    <img src={assets.Rotate_arrow} alt="" className="w-5 h-4 cursor-pointer"/>
+                  <div className="flex bg-purple-400 text-slate-100 py-4 px-3 gap-4 rounded-xl">
+                    <img
+                      src={assets.user_icon}
+                      alt="Gemini Icon"
+                      className="w-7 h-7 border-[1.5px] lg:w-10 lg:h-10 rounded-full"
+                    />
+                    <p
+                      dangerouslySetInnerHTML={{ __html: resultData }}
+                      className="text-[13px]  lg:text-[20px] w-[60vw] text-start leading-2 lg:pt-2 font-normal lg:leading-6"
+                    ></p>
                   </div>
-                <p>
-                <p className="mt-auto align-text-bottom"> {showTime}</p></p>
-                </div>
+                  <div className="flex justify-between">
+                    <div className="flex gap-4 py-2">
+                      <CopyToClipboard
+                        text={resultData}
+                        onCopy={() => alert("Copied!")}
+                      >
+                        <img
+                          src={assets.Copy_text}
+                          alt="Copy Icon"
+                          className="w-4 h-4 lg:w-5 lg:h-5 cursor-pointer"
+                        />
+                      </CopyToClipboard>
+                      <AiOutlineLike className="cursor-pointer text-sm" />
+                      <HiOutlineSpeakerWave
+                        className="cursor-pointer"
+                        onClick={() => Convert_to_speech()}
+                      />
+                      <img
+                        src={assets.Rotate_arrow}
+                        alt="Rotate Arrow"
+                        className="w-4 h-3 lg:w-5 lg:h-4 cursor-pointer"
+                      />
+                    </div>
+                    <p className="mt-auto text-sm align-text-bottom">
+                      {showTime}
+                    </p>
+                  </div>
                 </div>
               )}
             </div>
           </div>
         ) : (
           <>
-            <div className="greet mx-32 text-6xl  font-medium p-5">
-              
-                <span className="bg-gradient-to-r from-blue-500 to-red-500 bg-clip-text text-transparent">
-                  Welcome To Ani-gpt.
-                </span>
-              
-              <p className="text-lg mx-96 pt-4 w-96 font-normal text-[#7a7373]">Completely transforming your relationship with AI, providing a richer, more customized, and fluid interaction that adapts to your needs and preferences.</p>
+            <div className="greet lg:mx-32 text-4xl lg:text-6xl font-medium p-5">
+              <span className="bg-gradient-to-r from-blue-500 to-red-500 bg-clip-text text-transparent">
+                Welcome To Ani-gpt.
+              </span>
+              <p className="hidden lg:block lg:text-xl text-center text-wrap pt-4 px-96 font-normal text-[#7a7373]">
+                Completely transforming your relationship with AI, providing a
+                richer, more customized, and fluid interaction that adapts to
+                your needs and preferences.
+              </p>
             </div>
-            <div className="mb-10 flex flex-col items-center gap-1">
+            <div className="lg:mb-10 lg:block flex-col items-center  lg:gap-4">
               {[
                 {
                   text: "Show me how to build something by hand",
@@ -115,14 +176,16 @@ const Main = () => {
               ].map((card, index) => (
                 <div
                   key={index}
-                  className="card hover:bg-slate-200 w-[30vw] p-4 rounded-xl relative cursor-pointer flex items-center gap-4"
+                  className="card hover:bg-slate-200 shadow-lg  border-t-2 mx-3 w-auto lg:w-[30vw] px-3 p-2 lg:p-4 lg:mx-[30vw] rounded-xl relative cursor-pointer flex  items-center gap-2 mb-2"
                 >
                   <img
-                    className="w-9 p-1 bg-white rounded-3xl"
+                    className="w-7 lg:w-9 p-1 border-2 bg-white rounded-3xl"
                     src={card.icon}
                     alt="Card Icon"
                   />
-                  <p className="text-gray-600 text-lg font-medium">{card.text}</p>
+                  <p className="text-gray-600 text-[10px] lg:text-lg font-medium">
+                    {card.text}
+                  </p>
                 </div>
               ))}
             </div>
@@ -130,19 +193,19 @@ const Main = () => {
         )}
 
         {/* Bottom Input Section */}
-        <div className="main-bottom absolute bottom-0 w-full max-w-[85vw] py-5 px-0 m-auto">
-          <div className="flex items-center gap-4 w-full">
+        <div className="main-bottom absolute bottom-0  lg:max-w-[90vw] pb-3 lg:py-5 m-auto">
+          <div className="flex  items-center gap-2 w-full">
             {/* Search Box */}
-            <div className="search-box flex items-center flex-1 gap-3 bg-white px-4 py-2 border-2 border-gray-400 rounded-full">
+            <div className="search-box flex  items-center gap-1 bg-white px-4 py-2 lg:py-3 border-2 border-gray-400 rounded-full">
               <input
                 onChange={(e) => setinput(e.target.value)}
                 value={input}
-                className="bg-transparent flex-1 border-none outline-none p-2 py-1 text-lg"
+                className="bg-transparent text-[15px] flex-1 w-[60vw] border-none outline-none lg:p-2 lg:py-1 lg:text-lg lg:w-[83vw]"
                 type="text"
                 placeholder="Enter your prompt"
               />
               <img
-                className="w-8 cursor-pointer"
+                className="w-6 lg:w-8 cursor-pointer"
                 src={assets.mic_icon}
                 alt="Microphone Icon"
               />
@@ -150,14 +213,18 @@ const Main = () => {
             {/* Send Icon */}
             <div
               onClick={onSent}
-              className="w-14 h-14 cursor-pointer bg-gradient-to-t from-green-400 to-blue-300 rounded-full shadow-md flex items-center justify-center"
+              className="w-10 h-10 lg:w-14 lg:h-14 cursor-pointer bg-gradient-to-t from-green-400 to-blue-300 rounded-full shadow-md flex items-center justify-center"
             >
-              <img className="w-8" src={assets.send_icon} alt="Send Icon" />
+              <img
+                className="w-6 lg:w-8"
+                src={assets.send_icon}
+                alt="Send Icon"
+              />
             </div>
           </div>
-          <p className="bottom-info text-center pt-2 text-sm text-gray-400">
-            Gemini may display inaccurate info, double-check your responses before
-            using them.
+          <p className="bottom-info hidden lg-block pt-2 text-[10px] lg:text-base text-gray-400">
+            Gemini may display inaccurate info, double-check your responses
+            before using them.
           </p>
         </div>
       </div>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,50 +1,55 @@
-import React, { useContext, useState } from 'react';
-import { assets } from '../../assets/assets';
-import { Context } from '../../context/Context';
+import React, { useContext, useState } from "react";
+import { assets } from "../../assets/assets";
+import { Context } from "../../context/Context";
 
 const Sidebar = () => {
-    const [extend, setExtend] = useState(false);
-    const { onSent, prevPrompt, setRecentPrompt } = useContext(Context); // Default to empty array
-    return (
-        <div className='sidebar inline-flex w-auto flex-col justify-between bg-slate-100 h-screen py-6 px-4 text-lg font-medium'>
-            <div className='top'>
-                <img onClick={() => setExtend(prev => !prev)} src={assets.menu_icon} className='menu w-5 block cursor-pointer' alt="" />
-                <div className='new-chat mt-10 inline-flex items-center gap-3 px-4 py-3 bg-slate-200 text-sm text-gray-600 cursor-pointer rounded-[50px]'>
-                    <img src={assets.plus_icon} className='w-5' alt="" />
-                    {extend ? <p>New Chat</p> : null}
-                </div>
-
-                {extend ? (
-                    <div className="recent flex flex-col">
-                        <p className="recent-title mt-8 mb-5">Recent</p>
-                        {prevPrompt.map((item, index) => (
-                            <div
-                                key={index} 
-                                className="recent-entry flex items-start gap-3 p-3 pr-10 rounded-[50px] text-[#282828] cursor-pointer hover:bg-slate-200"
-                            >
-                                <img src={assets.message_icon} className="w-5" alt="" />
-                                <p className='w-28'>{item}...</p> 
-                            </div>
-                        ))}
-                    </div>
-                ) : null}
-            </div>
-            <div className='bottom flex flex-col'>
-                <div className="bottom-item recent-entry flex items-start gap-3 p-3 pr-10 rounded-[50px] text-[#282828] cursor-pointer hover:bg-slate-200">
-                    <img src={assets.question_icon} className='w-5' alt="" />
-                    {extend ? <p>Help</p> : null}
-                </div>
-                <div className="bottom-item recent-entry flex items-start gap-3 p-3 pr-10 rounded-[50px] text-[#282828] cursor-pointer hover:bg-slate-200">
-                    <img src={assets.history_icon} className='w-5' alt="" />
-                    {extend ? <p>Activity</p> : null}
-                </div>
-                <div className="bottom-item recent-entry flex items-start gap-3 p-3 pr-10 rounded-[50px] text-[#282828] cursor-pointer hover:bg-slate-200">
-                    <img src={assets.setting_icon} className='w-5' alt="" />
-                    {extend ? <p>Settings</p> : null}
-                </div>
-            </div>
+  const [extend, setExtend] = useState(false);
+  const { onSent, prevPrompt, setRecentPrompt } = useContext(Context); // Default to empty array
+  return (
+    <div className="hidden  sidebar lg:inline-flex w-auto flex-col justify-between bg-slate-100 h-screen py-6 px-4 text-lg font-medium">
+      <div className="top">
+        <img
+          onClick={() => setExtend((prev) => !prev)}
+          src={assets.menu_icon}
+          className="menu w-5 block cursor-pointer"
+          alt=""
+        />
+        <div className="new-chat mt-10 inline-flex items-center gap-3 px-4 py-3 bg-slate-200 text-sm text-gray-600 cursor-pointer rounded-[50px]">
+          <img src={assets.plus_icon} className="w-5" alt="" />
+          {extend ? <p>New Chat</p> : null}
         </div>
-    );
+
+        {extend ? (
+          <div className="recent flex flex-col">
+            <p className="recent-title mt-8 mb-5">Recent</p>
+            {prevPrompt.map((item, index) => (
+              <div
+                key={index}
+                className="recent-entry flex items-start gap-3 p-3 pr-10 rounded-[50px] text-[#282828] cursor-pointer hover:bg-slate-200"
+              >
+                <img src={assets.message_icon} className="w-5" alt="" />
+                <p className="w-28">{item}...</p>
+              </div>
+            ))}
+          </div>
+        ) : null}
+      </div>
+      <div className="bottom flex flex-col">
+        <div className="bottom-item recent-entry flex items-start gap-3 p-3 pr-10 rounded-[50px] text-[#282828] cursor-pointer hover:bg-slate-200">
+          <img src={assets.question_icon} className="w-5" alt="" />
+          {extend ? <p>Help</p> : null}
+        </div>
+        <div className="bottom-item recent-entry flex items-start gap-3 p-3 pr-10 rounded-[50px] text-[#282828] cursor-pointer hover:bg-slate-200">
+          <img src={assets.history_icon} className="w-5" alt="" />
+          {extend ? <p>Activity</p> : null}
+        </div>
+        <div className="bottom-item recent-entry flex items-start gap-3 p-3 pr-10 rounded-[50px] text-[#282828] cursor-pointer hover:bg-slate-200">
+          <img src={assets.setting_icon} className="w-5" alt="" />
+          {extend ? <p>Settings</p> : null}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
